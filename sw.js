c7b822c1d7bb2d5e5b70484dc31d5c4466de992a
rodrigodrg1 <rodrigozqs@gmail.com>
2026-08-17 12:53:24 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+
+const CACHE='meu-croche-pwa-v1';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+
+  const url=new URL(req.url);
+
+  // External libraries: try network first, fall back to cache if already seen.
+  if(url.origin!==location.origin){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(req,copy));
+        return res;
+      }).catch(()=>caches.match(req))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached=>cached || fetch(req).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy));
+      return res;
+    }))
+  );
+});
