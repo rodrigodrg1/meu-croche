655e1a5ab72e691b00c34bfd870682243ee05934
rodrigodrg1 <rodrigozqs@gmail.com>
2026-08-20 17:20:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,56 +1,51 @@
-const CACHE_NAME = 'meu-croche-v7-6-7-mobile-final-20260820-02';
-const CORE = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME='meu-croche-v7-6-7-estavel-20260820-03';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install',event=>{
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
-      Promise.all(CORE.map(url => cache.add(url).catch(() => null)))
+      Promise.all(CORE.map(url=>cache.add(url).catch(()=>null)))
     )
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate',event=>{
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
 
-  if (event.request.mode === 'navigate') {
+  if(event.request.mode==='navigate'){
     event.respondWith(
-      fetch(event.request, {cache:'no-store'})
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy)).catch(()=>{});
+      fetch(event.request,{cache:'no-store'})
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE_NAME).then(cache=>cache.put('./index.html',copy)).catch(()=>{});
           return response;
         })
-        .catch(() => caches.match('./index.html'))
+        .catch(()=>caches.match('./index.html'))
     );
     return;
   }
 
-  const url = new URL(event.request.url);
-  if (url.origin === self.location.origin) {
+  const url=new URL(event.request.url);
+  if(url.origin===self.location.origin){
     event.respondWith(
-      caches.match(event.request).then(cached =>
-        cached || fetch(event.request).then(response => {
-          if (response && response.ok) {
-            caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone())).catch(()=>{});
+      caches.match(event.request).then(cached=>{
+        const network=fetch(event.request).then(response=>{
+          if(response&&response.ok){
+            caches.open(CACHE_NAME).then(cache=>cache.put(event.request,response.clone())).catch(()=>{});
           }
           return response;
-        })
-      )
+        }).catch(()=>cached);
+        return cached||network;
+      })
     );
   }
 });
